8802c2fec68192928a7cf0213df1769c59a4f432
raghavbahukhandi <raghapb34n@gmail.com>
2022-07-18 01:05:48 +0530

    maybe

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.file_watcher = ActiveSupport::FileUpdateChecker
 
   # Use a different logger for distributed setups.
   # require "syslog/logger"
